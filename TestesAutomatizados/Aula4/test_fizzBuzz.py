df76013ff6a46f98f2bef34cde2af10e84634423
Guilherme Sampaio Dias <gusdias@rd.com.br>
2024-10-13 11:53:58 -0300

    primeira parte ap2 teste

--- TestesAutomatizados/Aula4/test_fizzBuzz.py
+++ TestesAutomatizados/Aula4/test_fizzBuzz.py
@@ -63,6 +63,10 @@ def val19():
 def val20():
     return fizBuzz(20)
 
+# @pytest.fixture
+# def unico():
+#     for i in range(20):
+
 
 
 
@@ -125,3 +129,27 @@ def test19(val19):
 
 def test20(val20):
     assert val20 == 'Buzz'
+
+def comFor():
+    return {i : fizBuzz(i) for i in range(1, 21) }
+
+    
+
+@pytest.fixture
+def fizzbuzz_values():
+    print( {i:fizBuzz(i) for i in range(1, 21)})
+
+@pytest.mark.parametrize("num, expected", [
+    (1, 1), (2, 2), (3, 'Fizz'),(4, 4),
+    (5, 'Buzz'),(6, 'Fizz'),
+    (7, 7),(8, 8),
+    (9, 'Fizz'),(10, 'Buzz'),
+    (11, 11),(12, 'Fizz'),
+    (13, 13), (14, 14),
+    (15, 'FizzBuzz'), (16, 16),
+    (17, 17),  (18, 'Fizz'),
+    (19, 19),
+    (20, 'Buzz'),
+])
+def test_fizzbuzz(fizzbuzz_values, num, expected):
+    assert fizzbuzz_values[num] == expected
